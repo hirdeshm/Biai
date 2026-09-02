4ebcea90b982e9e1bccf738f48ca69d0cd6e37f8
hirdeshm <hirdeshfiles4444@gmail.com>
2026-09-02 15:10:38 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,8 @@ app = FastAPI(title="BusinessIntelligence.ai")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000","https://biai-beryl.vercel.app"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000",
+                   "https://biai-beryl.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
