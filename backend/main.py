a610c8db54fd8142eb36946321578e1a047a5d50
hirdeshm <hirdeshfiles4444@gmail.com>
2026-09-02 19:15:02 +0530

    update CORS 0.1

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000",
                     "http://127.0.0.1:3000",
-                   "https://biai-dqw9.vercel.app/"],
+                   "https://biai-dqw9.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
